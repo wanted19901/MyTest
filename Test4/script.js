2b8765b373d58673c3ca024dd2e921d2090558cd
wanted19901 <wanted19901@gmail.com>
2020-06-24 20:29:37 +0300

    use forEach

--- Test4/script.js
+++ Test4/script.js
@@ -1,17 +1,29 @@
-let myStr = prompt("Введите строку","JavaScript_is_awesome")
-function toCamelCase(str){
-str = str.toLowerCase();
-let newArr = [];
-let arr = str.split("_");
-for (el of arr){
- if (el === arr[0]){ 
-	newArr.push(el)
-	continue
-	} 
-	else newStr = el[0].toUpperCase() + el.slice(1);
-	newArr.push(newStr);
-	}
-	result = newArr.join("");
-}
+ let myStr = prompt("Введите строку","JavaScript_is_awesome")
+// function toCamelCase(str){
+// str = str.toLowerCase();
+// let newArr = [];
+// let arr = str.split("_");
+// for (el of arr){
+//  if (el === arr[0]){ 
+// 	newArr.push(el)
+// 	continue
+// 	} 
+// 	else newStr = el[0].toUpperCase() + el.slice(1);
+// 	newArr.push(newStr);
+// 	}
+// 	result = newArr.join("");
+// }
 
 toCamelCase(myStr)
+
+function toCamelCase(str){
+	let newArr = [];
+	let arr = str
+	.toLowerCase()
+	.split("_"); 
+arr.forEach(function(item, i ) {
+	(i==0)?	newArr.push(item) : (item = item.charAt(0).toUpperCase() + item.slice(1) , newArr.push(item));
+	result = newArr.join("");
+  });
+  console.log(result)
+}
