792b750d414e645c1cae2d5e398a6f3e979c332f
wanted19901 <wanted19901@gmail.com>
2020-06-26 23:42:03 +0300

    Test10 Calc

---
+++ Test10/script.js
@@ -0,0 +1,25 @@
+//let numInput = document.querySelector('.login'); //окно ввода
+let num1 = document.querySelector('.container .row .col-md-3 .num1'); // кнопка
+let num2= document.querySelector('.container .row .col-md-3 .num2');
+let num3 = document.querySelector('.container .row .col-md-3 .num3');
+let num4 = document.querySelector('.container .row .col-md-3 .num4');
+let num5 = document.querySelector('.container .row .col-md-3 .num5');
+let num6 = document.querySelector('.container .row .col-md-3 .num6');
+let num7 = document.querySelector('.container .row .col-md-3 .num7');
+let num8 = document.querySelector('.container .row .col-md-3 .num8');
+let num9 = document.querySelector('.container .row .col-md-3 .num9');
+let num0 = document.querySelector('.container .row .col-md-6 .num0');
+let res = document.querySelector('.container .row .col-md-3 .num0');
+let C = document.querySelector('.container .row .col-md-3 .c');
+let Some= document.querySelector('.container .row .col-md-3 .num0');
+let division = document.querySelector('.container .row .col-md-3 .division');
+let mult = document.querySelector('.container .row .col-md-3 .mult');
+let minus = document.querySelector('.container .row .col-md-3 .minus');
+let plus = document.querySelector('.container .row .col-md-3 .plus');
+let dot = document.querySelector('.container .row .col-md-3 .dot');
+//let chek = document.querySelector('.chek');
+
+num1.onclick = () => { 
+    alert("123")
+    
+  }
--- Test8/script.js
+++ Test8/script.js
@@ -1,14 +1,2 @@
-var obj = {
-  salary: 30,
-  age : 20,
-  name : "John",
-  date : 15
-}
-let sum = 0;
-for (let key in obj) { 
-  if ((typeof obj[key]) == "number"){
-    obj[key]*=2;
-    sum += obj[key] ;    
-  }
-}
-console.log(sum)
+let numInput = document.querySelector('.login'); //окно ввода
+let chek = document.querySelector('.chek'); // кнопка
