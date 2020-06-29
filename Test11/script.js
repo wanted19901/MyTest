fa71460926a605b6c9933880bbc34d1ad1af3a18
wanted19901 <wanted19901@gmail.com>
2020-06-29 17:53:00 +0300

    working application on Test11

--- Test11/script.js
+++ Test11/script.js
@@ -1,30 +1,21 @@
-let input1 = document.querySelector('.login'); //окно ввода
-let chek = document.querySelector('.chek'); // кнопка
 google.maps.event.addDomListener(window, 'load', init);
-
 var autocompletes, marker, infowindow, map;
 function init() {
-   
-        var map = new google.maps.Map(document.getElementById('map'), {
-            center: {lat: -33.8688, lng: 151.2195},
-            zoom: 13
-            });
-            infowindow = new google.maps.InfoWindow();
-            marker = new google.maps.Marker({
-            map: map
-            });
-    }
-
+  var map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: -33.8688, lng: 151.2195},
+      zoom: 13
+      });
+  infowindow = new google.maps.InfoWindow();
+  marker = new google.maps.Marker({
+  map: map
+ });
+}
 var inputs = document.querySelector('.login');
 autocompletes = new google.maps.places.Autocomplete(inputs);
-
-
-
 google.maps.event.addListener(autocompletes, 'place_changed', function () {
     marker.setVisible(false);
     infowindow.close();
-    
-    
+           
     var place = autocompletes.getPlace();
     if (!place.geometry) {
     window.alert("Error");
@@ -36,27 +27,40 @@ google.maps.event.addListener(autocompletes, 'place_changed', function () {
     }));
     marker.setPosition(place.geometry.location);
     marker.setVisible(true);
-               
-    let city = place.formatted_address
-    let cityGeometry = place.geometry.location
-    let location = document.querySelector('#location');
-    location.textContent = city 
-    var d = new Date(); 
-    let dateNow = document.querySelector('#date');
-    dateNow.textContent = d.getDate()  + "." + (d.getMonth()+1) + "." + d.getFullYear()
+         
+let latitude = place.geometry.location.lat();
+let longitude = place.geometry.location.lng();
+console.log(latitude, longitude)
+   
+async function getWeather(latitude, longitude) {
+let weatheUrl = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${latitude}&lon=${longitude}&key=06bfa05643c54c6eaf6c26c7b39abd7d`;    
+let response = await fetch(weatheUrl)
+var json = await response.json();
+console.log(json)
+console.log(json.data[0].temp, json.data[0].weather.description)
     
-
-    console.log(cityGeometry, city) 
-    console.log(place)
-      
-
-    let latitude = place.geometry.location.lat();
-    let longitude = place.geometry.location.lng();
-    console.log(latitude, longitude)
-    
-    var apiURI2 = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${latitude}&lon=${longitude}&key=06bfa05643c54c6eaf6c26c7b39abd7d`;
-    setTimeout( () =>{
-    let json = JSON.stringify(apiURI2);
-    console.log(apiURI2)}, 3000 )
+for (let i=0; i <= json.data.length-1; i++) {
   
-    });
+   var dateElem = document.createElement('tr');
+   //dateElem.style = 'border: 1px solid black; width: 100px'
+   var em = document.createElement('em');
+   dateElem.append(` ${json.data[i].valid_date} `);
+   var defaultElem = document.querySelector("#date");
+   defaultElem.append(dateElem);
+ 
+  var tempElem = document.createElement('tr');
+  tempElem.append(` ${json.data[i].temp} `);
+  var defaultDate = document.querySelector("#curTemp");
+  defaultDate.append(tempElem);
+  
+  var weatherElem = document.createElement('tr');
+  weatherElem.append(` ${json.data[i].weather.description}`);
+  var defaultWeather = document.querySelector("#weather");
+  defaultWeather.append(weatherElem);
+  
+    
+  }
+} 
+getWeather(latitude, longitude)
+ 
+});
