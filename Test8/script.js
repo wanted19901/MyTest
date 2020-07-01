f11a8c8969e46ff4efb098f76db4193f02904c30
wanted19901 <wanted19901@gmail.com>
2020-07-01 11:47:07 +0300

    repair test8

--- Test8/script.js
+++ Test8/script.js
@@ -1,2 +1,14 @@
-let numInput = document.querySelector('.login'); //окно ввода
-let chek = document.querySelector('.chek'); // кнопка
+var obj = {
+    salary: 30,
+    age : 20,
+    name : "John",
+    date : 15
+  }
+  let sum = 0;
+  for (let key in obj) { 
+    if ((typeof obj[key]) == "number"){
+      obj[key]*=2;
+      sum += obj[key] ;    
+    }
+  }
+  console.log(sum) 
