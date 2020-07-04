cd4c76bb7a0b0d3d9014023a12ce6c42cbdf32fe
wanted19901 <wanted19901@gmail.com>
2020-07-04 10:25:52 +0300

    Revision of the verification template

--- Test5/script.js
+++ Test5/script.js
@@ -2,8 +2,8 @@ let numInput = document.querySelector('.login');
 let chek = document.querySelector('.chek');
 numInput.value = "А234КХ23"
 let str = numInput.value
-let patern = /[АВЕКМНОРСТУХ]\d{3}[АВЕКМНОРСТУХ]{2}\d{2,3}/;
+let patern = /^[АВЕКМНОРСТУХ]\d{3}[АВЕКМНОРСТУХ]{2}\d{2,3}$/;
 chek.onclick = () => {
-   console.log(patern.test(str))
+   console.log(patern.test(numInput.value))
     
 }
