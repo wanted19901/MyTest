e0cb00aa64d0bd25c7b29d4e847e5bc30373857c
wanted19901 <wanted19901@gmail.com>
2020-07-04 11:11:33 +0300

    function fix

--- Test7/script.js
+++ Test7/script.js
@@ -1,18 +1,18 @@
 let arr = [];
 function arrIncluder(min, max){
-  for (let i=min; i<= max ;i++) {
-    arr.push(min);
-    min++;
-   }
+  for (let i=min; i<= max; i++) {
+    arr.push(i);
+  }
 }
 function isPrime(num){
-if (num%3==1 ) {
-  return true 
-   }
- else {
-  return false;
-   }
+  if (num%3 == 1) {
+    return true 
+  }
+  else {
+    return false
+  }
 } 
-arrIncluder(4,16)
+arrIncluder(1,20)
 let cloneArr = arr.slice();
-console.log(cloneArr.filter(isPrime))
+console.log(arr)                                  // Default array
+console.log(cloneArr.filter(isPrime))             // Only prime number array
