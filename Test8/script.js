fb107c7beba90630067bb08d85f0a5fbf5b6943e
wanted19901 <wanted19901@gmail.com>
2020-07-04 11:15:39 +0300

    Codestyle fix

--- Test8/script.js
+++ Test8/script.js
@@ -1,14 +1,14 @@
 var obj = {
-    salary: 30,
-    age : 20,
-    name : "John",
-    date : 15
+  salary: 30,
+  age : 20,
+  name : "John",
+  date : 15
+}
+let sum = 0;
+for (let key in obj) { 
+  if ((typeof obj[key]) == "number"){
+    obj[key]*=2;
+    sum += obj[key] ;    
   }
-  let sum = 0;
-  for (let key in obj) { 
-    if ((typeof obj[key]) == "number"){
-      obj[key]*=2;
-      sum += obj[key] ;    
-    }
-  }
-  console.log(sum) 
+}
+console.log(sum) //Sum of numeric properties
