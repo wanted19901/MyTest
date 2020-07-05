791d210ec61b2806e551ab4444e6e06a22c76641
wanted19901 <wanted19901@gmail.com>
2020-07-05 21:08:05 +0300

    codestyle fix

--- Test11 - jquery/script.js
+++ Test11 - jquery/script.js
@@ -1,60 +1,49 @@
-google.maps.event.addDomListener(window, 'load', init);
-var autocompletes, marker, infowindow, map;
+var autocompletes;
+var inputs = document.querySelector('.search');
+
 function init() {
-    var map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: -33.8688, lng: 151.2195 },
-        zoom: 13
-    });
-    infowindow = new google.maps.InfoWindow();
-    marker = new google.maps.Marker({
-        map: map
-    });
+    google.maps.event.addDomListener(window, 'load', init);
 }
-var inputs = document.querySelector('.login');
+
 autocompletes = new google.maps.places.Autocomplete(inputs);
 google.maps.event.addListener(autocompletes, 'place_changed', function () {
-    marker.setVisible(false);
-    infowindow.close();
+   
+    clearDisplay()
 
     var place = autocompletes.getPlace();
     if (!place.geometry) {
         window.alert("Error");
         return;
     }
-    marker.setIcon(({
-        url: place.icon,
-        scaledSize: new google.maps.Size(35, 35)
-    }));
-    marker.setPosition(place.geometry.location);
-    marker.setVisible(true);
-
     let latitude = place.geometry.location.lat();
     let longitude = place.geometry.location.lng();
-    
-    async function getWeather(latitude, longitude) {
-        let weatheUrl = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${latitude}&lon=${longitude}&key=06bfa05643c54c6eaf6c26c7b39abd7d`;
-        let response = await fetch(weatheUrl)
-        var json = await response.json();
 
-        for (let i = 0; i <= json.data.length - 1; i++) {
-
-            var dateElem = document.createElement('tr');
-            dateElem.append(` ${json.data[i].valid_date} `);
-            var defaultElem = $("#date");
-            defaultElem.append(dateElem);
-
-            var tempElem = document.createElement('tr');
-            tempElem.append(` ${json.data[i].temp} `);
-            var defaultDate = $("#curTemp");
-            defaultDate.append(tempElem);
-
-            var weatherElem = document.createElement('tr');
-            weatherElem.append(` ${json.data[i].weather.description}`);
-            var defaultWeather = $("#weather");
-            defaultWeather.append(weatherElem);
-
-        }
-    }
-    getWeather(latitude, longitude)
+    getWeather(latitude, longitude);
 
 });
+
+async function getWeather(latitude, longitude) {
+   
+    let weatheUrl = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${latitude}&lon=${longitude}&key=06bfa05643c54c6eaf6c26c7b39abd7d`;
+    let response = await fetch(weatheUrl)
+    var json = await response.json();
+       
+        json.data.forEach(  el => {
+            $('tbody').append(`
+             <tr>
+              <td>${el.valid_date }</td>
+              <td>${el.temp}</td>
+              <td>${el.weather.description}</td>
+             </tr>
+            `)
+            
+        });
+}
+
+function clearDisplay(){
+    t1 = document.querySelector('#tbody');
+    while (t1.firstChild) {
+        t1.removeChild(t1.firstChild);
+    }
+        
+}
